15077d10c48268425d22a10c9a2ec1306a2c21cc
SjoerdGn <38759766+SjoerdGn@users.noreply.github.com>
2021-02-11 12:59:01 +0100

    cards added and game can end now #4

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let cardNum = 0;
 function range(start, end) {
   return Array(end - start + 1).fill().map((_, idx) => start + idx)
 }
-
+let numTurns = 34-3-10+1;
 let cardsPlayer = [];
 let cardsUnshuffled = range(4,36);
 let cards = cardsUnshuffled;
@@ -54,7 +54,6 @@ function startGame(){
   }
   getNames();
   cards = shuffle(cardsUnshuffled);
-  console.log(cards);
   document.getElementById('Started').innerHTML = "Started. Playing with " + numPlayers + " players."
   showCard();
 }
@@ -105,8 +104,16 @@ function takeCoins(){
   document.getElementById('total').innerHTML = totval;
   testZeroCoins();
   cardNum++;
+  console.log(numTurns);
+  console.log(cardNum);
+  if (cardNum == numTurns){
+    console.log("EndGame")
+    endGame();
+    document.getElementById("dropCoinButton").innerHTML = "<p> Don't you dare to press on any button. See the final scores below. </p>"
+  }
+  else {
   showCard();
-
+}
 }
 
 function takeCard(player){
@@ -118,11 +125,11 @@ function whoseTurnIsIt(){
 }
 
 function showCoins(){
-  var scores ="", i;
+  var wallets ="", i;
   for (i=0; i<numPlayers; i++) {
-    scores = scores + "<p> " + playerNames[i] + ", coins = " + vals[i] + ", cards = " + cardsPlayer[i].sort(function(a, b) {return a - b}) + " </p>";
+    wallets = wallets + "<p> " + playerNames[i] + ", coins = " + vals[i] + ", cards = " + cardsPlayer[i].sort(function(a, b) {return a - b}) + " </p>";
   }
-  document.getElementById("Coins").innerHTML = scores;
+  document.getElementById("Coins").innerHTML = wallets;
 }
 
 function testZeroCoins(){
@@ -133,4 +140,34 @@ function testZeroCoins(){
 
   }
 }
-//todo finish at certain turn
+
+
+function endGame(){
+  document.getElementById('currentCard').innerHTML = "<p style='font-size:30px'> <b>The game has ended!</b></p>";
+  let score = 0;
+  let scores = [];
+  var scoresString = "";
+  for (player=0;player<numPlayers; player++){
+    score = countScore(player);
+    scores.push(score);
+    scoresString = scoresString + "<p> " + playerNames[player] + ", score = " + score+ " </p>";
+  };
+  document.getElementById("showScoresAtEnd").innerHTML = scoresString;
+  //who has won?
+}
+
+function countScore(player){
+  let orderedCards = cardsPlayer[player].sort(function(a, b) {return a - b});
+  let orderedCardsCopy = orderedCards;
+  for (i=orderedCards.length;i>0; i--){
+    if(orderedCards[i] - orderedCards[i-1] == 1){
+      orderedCardsCopy[i] = 0;
+    }
+  };
+  let cardScore = orderedCardsCopy.reduce((a, b) => a + b, 0);
+  return cardScore - vals[player];
+
+}
+
+
+//show how many cards left
